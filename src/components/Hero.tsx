2ca7ae563b56c23abcc356710a1214f6bcf8402e
jubair-jx <jubair.official97@gmail.com>
2024-07-31 01:16:12 +0600

    health info field is working now

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -41,7 +41,7 @@ function Hero() {
               with our seamless and efficient platform.
             </p>
 
-            <Link href={"/book-seat"}>
+            <Link href={"/book-seat/personal-info"}>
               <button className="relative z-50 cursor-pointer inline-flex h-11 mt-6 overflow-hidden rounded-md p-[1.5px] focus:outline-none gap-2">
                 <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
                 <span className="inline-flex font-Kanit h-full w-full cursor-pointer items-center justify-center rounded-md bg-slate-900 px-7 py-1 font-medium text-white backdrop-blur-3xl text-xs sm:text-sm">
